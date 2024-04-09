88909844e0680796c603ccccca9130ec835757ef
Alice Abidine <abidinealice@gmail.com>
2024-04-09 17:45:21 +0200

    màj
    >modification du code sharp
    >modif try catch

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -7,18 +7,23 @@ const average = require('../utils/functions').average
 //USER CREATE A BOOK
 
 exports.createBook = (req, res, next) => {
-  const bookObject = JSON.parse(req.body.book);
-  delete bookObject._id;
-  delete bookObject.ratings;
-  delete bookObject.averageRating;
-  const book = new Book({
-      ...bookObject,
-      userId: req.auth.userId,
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.filepath}`
-  });
-  book.save()
-  .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
-  .catch(error => { res.status(400).json( { error })})
+  try {
+    const bookObject = JSON.parse(req.body.book);
+    delete bookObject._id;
+    delete bookObject.ratings;
+    delete bookObject.averageRating;
+    const book = new Book({
+        ...bookObject,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.filepath}`
+    });
+    book.save()
+    .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
+    .catch(error => { res.status(400).json( { error })})
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+  
 };
 
 //DISPLAY ALL BOOKS
@@ -61,6 +66,7 @@ exports.modifyBook = (req, res, next) => {
       ...JSON.parse(req.body.book),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.filepath}`
   } : { ...req.body };
+
   delete bookObject._userId;
   delete bookObject._id;
   delete bookObject.ratings;
@@ -83,7 +89,6 @@ exports.modifyBook = (req, res, next) => {
           }
       })
       .catch((error) => {
-        console.error(error);
         res.status(400).json({ error });
       });
 };
@@ -115,36 +120,40 @@ exports.deleteBook = (req, res, next) => {
 //USER RATE A BOOK
 
 exports.ratingBook = (req, res, next) => {
-  const newGrade = req.body.rating;
-  //Check if rating is between 0 and 5
-  if(!range(newGrade,0,5)){
-    res.status(400).json({message:`Error rating`});
-  }else{
-    const ratingObject = {...req.body, grade: req.body.rating};
-    delete ratingObject._id;
-    Book.findOne({ _id: req.params.id})
-    .then(book => {
-      const newRatings = book.ratings;
-      const userIds = newRatings.map(rating=>rating.userId.toString());
-      //Check if user has already rate the book
-      if(userIds.includes(req.auth.userId.toString())){
-        res.status(403).json({message: 'Not authorized !'});
-      }else{
-        //New rating added to ratings array
-        newRatings.push(ratingObject);
-        //Update average rating
-        const grades = newRatings.map(rating=>rating.grade);
-        const newAverageGrades = average(grades);
-        book.averageRating = newAverageGrades;
-        //Update book's ratings and average ratings in database
-        Book.updateOne({ _id: req.params.id}, { ratings: newRatings, averageRating: newAverageGrades})
-          //Returns update book
-          .then(() => {res.status(201).json(book);})
-          .catch((error) => {res.status(404).json({error: error});});
-      }})
-    .catch((error) => {
-      console.error(error);
-      res.status(404).json({ error });
-    });
+  try{
+    const newGrade = req.body.rating;
+    //Check if rating is between 0 and 5
+    if(!range(newGrade,0,5)){
+      res.status(400).json({message:`Error rating`});
+    }else{
+      const ratingObject = {...req.body, grade: req.body.rating};
+      delete ratingObject._id;
+      Book.findOne({ _id: req.params.id})
+      .then(book => {
+        const newRatings = book.ratings;
+        const userIds = newRatings.map(rating=>rating.userId.toString());
+        //Check if user has already rate the book
+        if(userIds.includes(req.auth.userId.toString())){
+          res.status(403).json({message: 'Not authorized !'});
+        }else{
+          //New rating added to ratings array
+          newRatings.push(ratingObject);
+          //Update average rating
+          const grades = newRatings.map(rating=>rating.grade);
+          const newAverageGrades = average(grades);
+          book.averageRating = newAverageGrades;
+          //Update book's ratings and average ratings in database
+          Book.updateOne({ _id: req.params.id}, { ratings: newRatings, averageRating: newAverageGrades})
+            //Returns update book
+            .then(() => {res.status(201).json(book);})
+            .catch((error) => {res.status(404).json({error: error});});
+        }})
+      .catch((error) => {
+        console.error(error);
+        res.status(404).json({ error });
+      });
+    }
+  }catch(error){
+    res.status(400).json({ error });
   }
 };
