23be8119262175879dffe5296c6f362ee06904ad
Alice Abidine <abidinealice@gmail.com>
2024-04-22 20:23:20 +0200

    màj final
    >add comments

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,16 +1,19 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const jwtRandom = process.env.JWT_SECRET;
- 
+
 module.exports = (req, res, next) => {
-   try {
-       const token = req.headers.authorization.split(' ')[1];
-       const decodedToken = jwt.verify(token, jwtRandom);
-       const userId = decodedToken.userId;
-       req.auth = {
-           userId: userId
-       };
-	next();
-   } catch(error) {
-       res.status(401).json({ error });
-   }
-}
+  try {
+    //Recovery token
+    const token = req.headers.authorization.split(" ")[1];
+    //Decode token
+    const decodedToken = jwt.verify(token, jwtRandom);
+    //Extraction userId
+    const userId = decodedToken.userId;
+    req.auth = {
+      userId: userId,
+    };
+    next();
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+};
